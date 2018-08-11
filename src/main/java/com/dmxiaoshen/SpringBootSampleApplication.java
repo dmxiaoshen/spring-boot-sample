b2076868b7bd70566affdcea00d9754c6c856c24
hushengen <hsg@dmxiaoshen.com>
2018-08-11 16:22:55 +0800

    modify

--- src/main/java/com/dmxiaoshen/SpringBootSampleApplication.java
+++ src/main/java/com/dmxiaoshen/SpringBootSampleApplication.java
@@ -2,12 +2,14 @@ package com.dmxiaoshen;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.dmxiaoshen.dao"})
 @ComponentScan({"com.dmxiaoshen"})
+@EnableAutoConfiguration
 public class SpringBootSampleApplication {
 
 	public static void main(String[] args) {
